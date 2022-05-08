8ac0804c5cf2da8de4aa6a21f0df1fcf6800015e
winston <winston@localhost.localdomain>
2022-05-08 11:46:06 +0800

    updated

---
+++ tool_gzip.py
@@ -0,0 +1,18 @@
+'''
+Created on 2022年5月8日
+
+@author: winston
+'''
+from gzip import GzipFile
+import io
+
+
+def gzip_compress(raw_data):
+    buf = io.BytesIO()
+    with GzipFile(mode='wb', fileobj=buf) as fp:
+        fp.write(raw_data)
+    return buf.getvalue()
+
+
+def gzip_decompress(raw_data):
+    return GzipFile(mode='rb', fileobj=io.BytesIO(raw_data)).read()
