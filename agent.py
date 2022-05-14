b4834e8ebf455bea37416a8205a5f406fe2e1b97
a18190 <a18190@qq.com>
2022-05-15 01:40:39 +0800

    'updated'

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 # -*- coding: utf8 -*-
 import random
 raw ='''
+Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)
 Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36
 '''
+# Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36
 #Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)
 
 agent_list = raw.strip().splitlines()
--- helper_net.py
+++ helper_net.py
@@ -49,8 +49,8 @@ def retry(attempt, fix_short_timeout=False):
 
 def get_with_random_agent_simple(*args, **kw):
     s = requests.Session()
-    # s.mount('http://', HTTPAdapter(max_retries=10))
-    # s.mount('https://', HTTPAdapter(max_retries=10))
+    s.mount('http://', HTTPAdapter(max_retries=10))
+    s.mount('https://', HTTPAdapter(max_retries=10))
 
     headers = {'user-agent': pick_one_agent()}
 
