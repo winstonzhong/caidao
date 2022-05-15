a18330d42ab9255166b58bbfa06f377b1dd2fc27
a18190 <a18190@qq.com>
2022-05-15 14:04:27 +0800

    'updated'

--- tools/django/abstract.py
+++ tools/django/abstract.py
@@ -5,17 +5,22 @@ Created on 2022年5月1日
 '''
 from django.db import models
 
-class AbstractModel(models.Model):
+class BaseModel(models.Model):
+    FIELDS = None
+    @classmethod
+    def get_fields(cls):
+        if not cls.FIELDS:
+            cls.FIELDS = list(map(lambda x: x.get_attname(), cls._meta.fields))
+        return cls.FIELDS
+    class Meta:
+        abstract = True
+
+class AbstractModel(BaseModel):
     update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
     create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
 
     class Meta:
         abstract = True
 
-    FIELDS = None
     
-    @classmethod
-    def get_fields(cls):
-        if not cls.FIELDS:
-            cls.FIELDS = list(map(lambda x: x.get_attname(), cls._meta.fields))
-        return cls.FIELDS
+    
