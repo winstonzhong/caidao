4d879fdf7eaec09719734689932f676397a33093
winston <winston@localhost.localdomain>
2022-06-09 11:38:21 +0800

    updated

--- tool_env.py
+++ tool_env.py
@@ -6,3 +6,14 @@ Created on 2022年6月3日
 import platform
 
 OS_WIN = platform.system() == 'Windows'
+
+
+def is_chinese(ch):
+    return '\u4e00' <= ch <= '\u9fff'
+
+
+def has_chinese(line):
+    for ch in line:
+        if is_chinese(ch):
+            return True
+    return False
