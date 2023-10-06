6ca1c6b5a76256fd0fd425465cd89eb271a58d24
liqiang <liq10@knownsec.com>
2023-10-06 15:04:11 +0800

    feat: taskOrder表新增是否下载字段

--- caidao_tools/django/chat_models.py
+++ caidao_tools/django/chat_models.py
@@ -237,7 +237,7 @@ class AbstractTaskOrder(models.Model):
     # parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, db_constraint=False, verbose_name="来源工单", blank=True, null=True)
 
     status = models.SmallIntegerField(verbose_name='状态', choices=STATUS, default=DEFAULT_STATUS)
-    priority = models.SmallIntegerField(verbose_name='状态', choices=PRIORITIES, default=DEFAULT_PRIORITY)
+    priority = models.SmallIntegerField(verbose_name='优先级', choices=PRIORITIES, default=DEFAULT_PRIORITY)
     user_requirement = models.CharField(max_length=50, verbose_name='客户要求', default='')
     kf_requirement = models.CharField(max_length=50, verbose_name='客服填写要求', default='')
     updated_at = models.DateTimeField(verbose_name='更新时间', auto_now=True)
@@ -249,6 +249,7 @@ class AbstractTaskOrder(models.Model):
                                         default=AbstractUser.PLATFORM_OA)
 
     pic_mode = models.SmallIntegerField(verbose_name='图像模式', choices=PIC_MODES, default=DEFAULT_PIC_MODE)
+    is_downloaded = models.BooleanField(verbose_name='是否下载', default=False)
 
     class Meta:
         abstract = True
