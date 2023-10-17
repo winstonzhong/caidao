676c3bf9e303811d5d542c793cf9a3d41b2fc1b6
a18190 <a18190@qq.com>
2023-10-17 10:53:57 +0800

    'updated'

---
+++ tool_url.py
@@ -0,0 +1,48 @@
+'''
+Created on 2023年10月17日
+
+@author: lenovo
+'''
+import os
+import re
+from urllib.parse import urlparse
+
+
+ptn_ftype = re.compile('http.+/format/(\w+)')
+
+def get_uuid_img_small_red_book(url):
+    '''
+    >>> get_uuid_img_small_red_book('http://ci.xiaohongshu.com/110/0/01e4b0260b46d6fe001000000001895014a4d5_0.jpg?imageView2/2/w/1080/format/jpg')
+    '/110/0/01e4b0260b46d6fe001000000001895014a4d5_0.jpg'
+    >>> get_uuid_img_small_red_book('http://ci.xiaohongshu.com/spectrum/1000g0k02b3ia1osh20005n4gogf4hna990kn1rg?imageView2/2/w/1080/format/jpg')
+    '/spectrum/1000g0k02b3ia1osh20005n4gogf4hna990kn1rg'
+    '''
+    return urlparse(url).path
+
+def get_ftype_img_small_red_book(url):
+    '''
+    >>> get_ftype_img_small_red_book('http://ci.xiaohongshu.com/110/0/01e4b0260b46d6fe001000000001895014a4d5_0.jpg?imageView2/2/w/1080/format/jpg')
+    'jpg'
+    >>> get_ftype_img_small_red_book('http://ci.xiaohongshu.com/spectrum/1000g0k02b3ia1osh20005n4gogf4hna990kn1rg?imageView2/2/w/1080/format/png')
+    'png'
+    '''
+    return ptn_ftype.match(url).groups()[0]
+
+def get_fpath_img_small_red_book(url, base='z:/tpl'):
+    '''
+    >>> get_fpath_img_small_red_book('http://ci.xiaohongshu.com/110/0/01e4b0260b46d6fe001000000001895014a4d5_0.jpg?imageView2/2/w/1080/format/jpg')
+    'z:/tpl/110/0/01e4b0260b46d6fe001000000001895014a4d5_0.jpg'
+    >>> get_fpath_img_small_red_book('http://ci.xiaohongshu.com/spectrum/1000g0k02b3ia1osh20005n4gogf4hna990kn1rg?imageView2/2/w/1080/format/png')
+    'z:/tpl/spectrum/1000g0k02b3ia1osh20005n4gogf4hna990kn1rg.png'
+    '''
+    fpath = get_uuid_img_small_red_book(url)
+    ftype = get_ftype_img_small_red_book(url)
+    if not fpath.endswith('.%s' % ftype):
+        fpath += '.%s' % ftype
+    return os.path.join(base, fpath[1:]).replace('\\', '/')
+    
+    
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod(verbose=False, report=False))
+    
