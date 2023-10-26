ec169a7198341e5a0bbad2f0d877e054866fca5c
a18190 <a18190@qq.com>
2023-10-26 16:51:53 +0800

    'updated'

--- tool_img.py
+++ tool_img.py
@@ -10,8 +10,18 @@ import cv2
 import numpy
 
 
-def img_to_base64url(img):
-    if img:
+def to_buffer(img):
+    if img is not None:
+        is_success, buffer = cv2.imencode(".png", img)
+        return buffer if is_success else None
+
+def cv2_to_base64url(img):
+    if img is not None:
+        content = base64.b64encode(to_buffer(img))
+        return 'data:image/png;base64,' + content.decode('utf8')
+
+def pil_to_base64url(img):
+    if img is not None:
         buf = io.BytesIO()
         img.save(buf, format='PNG')
         buf.seek(0)
