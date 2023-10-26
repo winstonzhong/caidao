22d5a2f51878bb9f30cde6513ee44910e749778a
a18190 <a18190@qq.com>
2023-10-26 14:30:44 +0800

    'updated'

--- tool_img.py
+++ tool_img.py
@@ -3,8 +3,20 @@ Created on 2023年10月19日
 
 @author: lenovo
 '''
+import base64
+import io
+
 import cv2
 import numpy
+
+
+def img_to_base64url(img):
+    if img:
+        buf = io.BytesIO()
+        img.save(buf, format='PNG')
+        buf.seek(0)
+        content = base64.b64encode(buf.getvalue())
+        return 'data:image/png;base64,' + content.decode('utf8')
 
 
 def show(img):
@@ -47,6 +59,7 @@ def to_black_white_by_upper_gray_value(img, v):
 def remove_background_by_upper_gray_value(img, v):
     gray = to_gray(img)
     mask = ((gray < v) * 255).astype(numpy.uint8)
+    # return mask
     mask = mono_to_rgb(mask)
     return cv2.bitwise_and(mask, img)
 
@@ -68,13 +81,43 @@ def put_water_mark(img, wm):
     # left = 0
     # right = w1
     a = img[top:bottom, left:right, ...]
-    
+    # return a
     # return a, wm
-    # r = a + wm
-    b = cv2.bitwise_or(wm, a)
-    img[top:bottom, left:right, ...] = b
+    # r = a.astype(numpy.int) + wm
+    # c = r.flatten()
+    # c = numpy.where(c>=255, 255, c).astype(numpy.uint8).reshape(wm.shape)
+    
+    b = cv2.bitwise_xor(wm.astype(numpy.int), a.astype(numpy.int))
+    img[top:bottom, left:right, ...] = b.astype(numpy.uint8)
     return img
+
+def get_canvas(width, height):
+    return numpy.zeros((height, width, 3), dtype=numpy.uint8)    
     
+def resize_image(img, width, height):
+    canvas = get_canvas(width, height)
+    h, w = img.shape[:2]
+    if h > w:
+        dsize = (int((height/h) * w), height)
+    else:
+        dsize = (width, int((width/w) * h))
+    # print(dsize)
+    img = cv2.resize(img,dsize=dsize,fx=None,fy=None,interpolation=cv2.INTER_LINEAR)
+    # return img
+    # max_len = max(img.shape[:2])
+    # if max_len > MAX_LENGTH:
+    #     ratio = MAX_LENGTH / max_len 
+    #     img = cv2.resize(img,dsize=None,fx=ratio,fy=ratio,interpolation=cv2.INTER_LINEAR)
+    # return force_exact_division(img)
+    h, w = img.shape[:2]
+    left = (width - w) // 2
+    top = (height - h) // 2
     
+    right = left+w
+    bottom = top+h
+    
+    canvas[top:bottom,left:right,...] = img
+    return canvas
+
     
     
