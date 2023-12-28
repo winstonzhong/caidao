a6d0892fd8b16618bdaf32fa1a473d74fba43f09
a18190 <a18190@qq.com>
2023-12-28 14:56:08 +0800

    'updated'

--- tool_ffmpeg.py
+++ tool_ffmpeg.py
@@ -6,6 +6,8 @@ Created on 2023年12月27日
 import re
 import subprocess
 
+from tool_env import is_number
+
 
 ptn_line = re.compile('(\d{2}):(\d{2}).(\d{3})')
 
@@ -13,11 +15,22 @@ def to_seconds(line):
     '''
     >>> to_seconds('02:28.400') == 148.4
     True
+    >>> to_seconds(148.11) == 148.11
+    True
     '''
-    l = ptn_line.match(line).groups()
-    return int(l[0]) * 60 + int(l[1]) + int(l[2]) / 1000
+    if not is_number(line):
+        l = ptn_line.match(line).groups()
+        line = int(l[0]) * 60 + int(l[1]) + int(l[2]) / 1000
+    return line
 
+def to_timestr(seconds):
+    '''
+    >>> to_timestr(148.4) == '02:28.400'
+    True
+    '''
+    return '%02d:%02d.%s' % (seconds //  60, int(seconds) % 60, ('%.03f' % seconds)[-3:])
 
+    
 
 def trim_audio(fpath_input, fpath_output, start=0, end=0):
     start_seconds = to_seconds(start)
