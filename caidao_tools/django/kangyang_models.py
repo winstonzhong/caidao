fc1d6337135684ffc236d55ccabbca368bc07a0b
liqiang <liq10@knownsec.com>
2024-11-28 17:06:44 +0800

    feat: 新增数据模型

--- caidao_tools/django/kangyang_models.py
+++ caidao_tools/django/kangyang_models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-from caidao_tools.django.abstract import AbstractModel
 from caidao_tools.django.storage import MyStorage
 
+
+# 用户数据来源
+MEASURE_SOURCE_CHOICES = (
+    (0, '用户输入数据'),
+    (1, '智能手环')
+)
 
 class AbstractUser(models.Model):
     """用户"""
@@ -184,3 +190,268 @@ class AbstractOrderDetail(models.Model):
         indexes = [
             models.Index(fields=['order_id']),
         ]
+
+
+class AbstractPatient(models.Model):
+    AUDIT_STATUS = (
+        (0, '初始创建'),
+        (1, '已获取用户基本信息'),
+        (2, '已获取用户手机号'),
+        (3, '已获取用户身份信息')
+    )
+    USER_GENDER = (
+        (0, '未知'),
+        (1, '男性'),
+        (2, '女性')
+    )
+    MARRIAGE = (
+        (0, "未填写"),
+        (1, "未婚"),
+        (2, "已婚"),
+        (3, "离异")
+    )
+
+    uuid = models.CharField(max_length=32, verbose_name="患者唯一id", db_index=True, unique=True)
+    name = models.CharField(max_length=15, verbose_name='患者姓名', null=True)
+    name_pinyin = models.CharField(max_length=128, verbose_name='患者姓名拼音', db_index=True, blank=True, null=True)
+    pinyin_target = models.CharField(max_length=5, verbose_name='姓名首字母标记', db_index=True, blank=True, null=True)
+    gender = models.SmallIntegerField(choices=USER_GENDER, verbose_name='用户性别', default=0)
+    height = models.SmallIntegerField(verbose_name="身高", help_text="单位：cm", blank=True, null=True)
+    weight = models.DecimalField(verbose_name="体重", help_text="单位：kg", max_digits=6, decimal_places=2, blank=True,
+                                 null=True)
+    marriage = models.SmallIntegerField(verbose_name="婚姻状况", choices=MARRIAGE, default=0)
+    social_card = models.CharField(verbose_name="社保卡号", null=True, blank=True, max_length=100)
+    social_card_num = models.CharField(verbose_name="社会保障号码", null=True, blank=True, max_length=200)
+    health_card_num = models.CharField(verbose_name="电子健康卡号", null=True, blank=True, max_length=200)
+    job = models.CharField(verbose_name="职业", max_length=32, blank=True, null=True)
+    id_card = models.CharField(max_length=18, verbose_name='身份证号', null=True)
+    birthday_date = models.DateField(null=True, blank=True, verbose_name='出生年月')
+    phone = models.CharField(max_length=12, verbose_name="手机号", null=True, blank=True)
+    city = models.CharField(max_length=32, verbose_name="城市", null=True, blank=True)
+    country = models.CharField(max_length=32, verbose_name="国家", null=True, blank=True)
+    province = models.CharField(max_length=32, verbose_name="省份", null=True, blank=True)
+    active = models.BooleanField(default=True, help_text='隐式删除', verbose_name="激活/禁用")
+    address = models.CharField(max_length=255, verbose_name="地址", null=True, blank=True)
+
+    allergy_history = models.CharField(max_length=2000, verbose_name='过敏史简介', null=True, blank=True)
+    past_medical_history = models.CharField(max_length=2000, verbose_name='既往病史简介', null=True, blank=True)
+    contagion = models.CharField(max_length=2000, verbose_name='传染病', null=True, blank=True)  # 肺结核, 肝炎, 其他
+    exposure_history = models.CharField(max_length=2000, verbose_name='暴露史', null=True, blank=True)  # 化学品, 毒物, 射线
+    family_medical_history = models.CharField(max_length=2000, verbose_name='家族史', null=True, blank=True)  # 父亲, 母亲,
+    genetic_disease_history = models.CharField(max_length=2000, verbose_name='遗传病史', null=True, blank=True)
+    disability = models.CharField(max_length=2000, verbose_name='残疾情况', null=True,
+                                  blank=True)  # 视力残疾, 听力残疾, 语言残疾, 肢体残疾, 智力残疾, 精神残疾, 其他残疾
+    wx_user_id = models.PositiveIntegerField(verbose_name='微信用户ID', blank=True, null=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+
+
+class AbstractMedicalHistory(models.Model):
+    CREATOR_TYPE_SYS = 0  # 系统
+    CREATOR_TYPE_PATIENT = 1  # 患者
+    # CREATOR_TYPE_DOCTOR = 2  # 医生
+    # CREATE_TYPE_PATIENT_SYNC = 3  # 患者一键同步
+    CREATOR_TYPE = (
+        (CREATOR_TYPE_SYS, "系统自动生成"),
+        (CREATOR_TYPE_PATIENT, "患者"),
+        # (CREATOR_TYPE_DOCTOR, "医生"),
+        # (CREATE_TYPE_PATIENT_SYNC, "患者一键同步")
+    )
+
+    CATE_NORMAL = 0
+    CATE_SUMMARY = 1
+    CATE_MEASURE = 2
+    CATE = (
+        (CATE_NORMAL, "健康档案"),
+        (CATE_SUMMARY, "健康小结"),
+        (CATE_MEASURE, "健康测评"),
+    )
+
+    treatment_date = models.DateField(help_text='就诊时间', null=True, blank=True)
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    # platform_patient = models.ForeignKey(patient_user_platform,on_delete=models.SET_NULL,null=True)
+    # platform = models.ForeignKey(Organization_Business_platform,on_delete=models.SET_NULL,null=True)
+    treatment_hospital = models.CharField(max_length=32, help_text='就诊医院', blank=True, null=True)
+    content = models.TextField(verbose_name='病情简介', blank=True, null=True)
+    creator_type = models.SmallIntegerField(choices=CREATOR_TYPE, default=CREATOR_TYPE_SYS, help_text='创建人')
+    # creator = models.SmallIntegerField(choices=CREATOR_TYPE,default=1,help_text='创建人')
+    # doctor_creator =models.ForeignKey(Doctor_User_WX,on_delete=models.DO_NOTHING,null=True)
+    # content_edit_time = models.DateTimeField(verbose_name="病情简介编辑时间",null=True)
+    diagnose_history = models.TextField(verbose_name="json保存会诊记录", null=True)
+    # hospital_create_time = models.CharField(max_length=32, help_text='医院创建患者报告时间', null=True)
+    diagnosis = models.TextField(verbose_name='诊断', default="")
+    treat_advice = models.TextField(verbose_name='患者填写诊疗意见', default="")  # 患者创建诊疗意见直接存入主表
+    cate = models.SmallIntegerField(choices=CATE, default=0, help_text='类型')
+    # channel_id = models.CharField(max_length=100,verbose_name='房间ID', default='')
+    is_deleted = models.BooleanField(verbose_name='是否删除', default=False)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = verbose_name_plural = '患者病历表'
+
+
+
+class AbstractMedicalHistoryAttachment(models.Model):
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    medical_history_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    Attach_file = models.FileField(verbose_name='文件地址', storage=MyStorage, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False, help_text='是否删除')
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = verbose_name_plural = '病历附件表'
+
+
+class AbstractTemperature(models.Model):
+    """体温"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.DecimalField(verbose_name="温度", help_text="单位: ℃", max_digits=3, decimal_places=1, default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '体温记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractBreath(models.Model):
+    """呼吸"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.SmallIntegerField(verbose_name="呼吸", help_text="单位: 次/min", default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '呼吸记录'
+        verbose_name_plural = verbose_name
+
+
+
+
+class AbstractPulse(models.Model):
+    """脉搏"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.SmallIntegerField(verbose_name="脉搏", help_text="单位: 次/min", default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '脉搏记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractBloodPressure(models.Model):
+    """血压"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    systolic_pressure = models.SmallIntegerField(verbose_name="收缩压", help_text="单位: mmHg", default=0)
+    diastolic_pressure = models.SmallIntegerField(verbose_name="舒张压", help_text="单位: mmHg", default=0)
+    heart_rate = models.SmallIntegerField(verbose_name="心率", help_text="单位: 次/min", default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '血压记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractBloodOxygen(models.Model):
+    """血氧"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.DecimalField(verbose_name="血氧", help_text="单位: %", max_digits=4, decimal_places=2, default=0)
+    perfusion_index = models.DecimalField(verbose_name="灌注指数", help_text="单位: %", max_digits=4, decimal_places=2, default=0)
+    heart_rate = models.SmallIntegerField(verbose_name="心率", help_text="单位: 次/min", default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '血氧记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractBloodGlucose(models.Model):
+    """血糖"""
+    MEASURE_CONDITION_CHOICES = (
+        (0, '空腹'),
+        (1, '餐后两小时'),
+        #
+        # (1, '早餐后'),
+        # (2, '午餐前'),
+        # (3, '午餐后'),
+        # (4, '晚餐前'),
+        # (5, '晚餐后'),
+        # (6, '睡前'),
+        # (7, '随机'),
+    )
+    DRUG_USE_CHOICES = (
+        (0, '用药前'),
+        (0, '用药后'),
+    )
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.DecimalField(verbose_name="血糖", help_text="单位: mmol/L", max_digits=6, decimal_places=2, default=0)
+    measure_condition = models.SmallIntegerField(verbose_name="测量状态", choices=MEASURE_CONDITION_CHOICES, default=0)
+    drug_use = models.SmallIntegerField(verbose_name="用药情况", choices=DRUG_USE_CHOICES, default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '血糖记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractUricAcid(models.Model):
+    """尿酸"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    value = models.SmallIntegerField(verbose_name="尿酸", help_text="单位: μmol/L", default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = '尿酸记录'
+        verbose_name_plural = verbose_name
+
+
+class AbstractBMI(models.Model):
+    """BMI"""
+    patient_id = models.PositiveIntegerField(verbose_name='患者ID', blank=True, null=True)
+    height = models.SmallIntegerField(verbose_name="身高", help_text="单位: cm", default=0)
+    weight = models.DecimalField(verbose_name="体重", help_text="单位: kg", max_digits=6, decimal_places=2, default=0)
+    measure_time = models.DateTimeField(verbose_name='更新时间', default=timezone.now)
+    source = models.SmallIntegerField(verbose_name='数据来源', choices=MEASURE_SOURCE_CHOICES, default=0)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        abstract = True
+        indexes = []
+        verbose_name = 'BMI记录'
+        verbose_name_plural = verbose_name
