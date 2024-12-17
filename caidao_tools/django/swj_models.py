cf6181f6a43e8f245d8f1aca08269c96aff7e9ff
a18190 <a18190@qq.com>
2024-12-17 23:35:46 +0800

    'updated'

--- caidao_tools/django/swj_models.py
+++ caidao_tools/django/swj_models.py
@@ -8,27 +8,42 @@ from django.db import models
 # Create your models here.
 
 class AbstractComputeTask(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
     data = models.BinaryField()
+    total = models.PositiveSmallIntegerField(null=True)
+    pl = models.FloatField(null=True)
 
     class Meta:
         abstract = True
 
 class AbstractMonoChain(models.Model):
     task_id = models.PositiveBigIntegerField()
+    name = models.CharField(max_length=20)
     data = models.BinaryField()
+    fraged = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
 
 class AbstractDnaFragment(models.Model):
+    task_id = models.PositiveBigIntegerField(null=True)
     chain_id = models.PositiveBigIntegerField()
     up = models.FloatField(null=True)
     low = models.FloatField(null=True)
-    attend = models.FloatField()
-    pl3 = models.FloatField()
-    pl5 = models.FloatField()
+    computed = models.BooleanField(default=False)
     
     class Meta:
         abstract = True
-    
+
+class AbstractDnaStrand(models.Model):
+    task_id = models.PositiveBigIntegerField(null=True)
+    fragment_ids = models.TextField(null=True)
+    attend = models.FloatField(null=True)
+    pl = models.FloatField(null=True)
+    pl_cut = models.FloatField(null=True)
+    level = models.PositiveSmallIntegerField(default=0)
+    expanded = models.BooleanField(default=False)
+    key = models.CharField(max_length=64, null=True)
+
+    class Meta:
+        abstract = True
--- tool_gzip.py
+++ tool_gzip.py
@@ -7,11 +7,16 @@ from gzip import GzipFile
 import io
 
 
+def write_in_chunks(gzip_file, data, chunk_size=1024*1024):  # 1MB chunk size
+    for i in range(0, len(data), chunk_size):
+        chunk = data[i:i+chunk_size]
+        gzip_file.write(chunk)
+
 def gzip_compress(raw_data):
     if raw_data:
         buf = io.BytesIO()
         with GzipFile(mode='wb', fileobj=buf) as fp:
-            fp.write(raw_data)
+            write_in_chunks(fp, raw_data)
         return buf.getvalue()
 
 
