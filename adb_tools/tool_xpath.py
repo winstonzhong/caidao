ec320aa9d6c5c9d24394a87cb9ba2eec09a09800
sam.guo <lovekarri@hotmail.com>
2025-02-22 08:28:48 +0800

    updated

--- adb_tools/tool_xpath.py
+++ adb_tools/tool_xpath.py
@@ -1,8 +1,9 @@
-'''
+"""
 Created on 2023年12月21日
 
 @author: lenovo
-'''
+"""
+
 import os
 import time
 
@@ -16,6 +17,7 @@ from tool_env import bounds_to_rect
 from tool_img import get_template_points, show, pil2cv2, cv2pil
 from lxml import etree
 
+
 class NoTemplatePopupException(Exception):
     pass
 
@@ -24,75 +26,76 @@ class DummyWatcher(object):
     def run(self, xml_content):
         return False
 
+
 class DummyDevice(object):
     def __init__(self, fpath=None, adb=None, xml=None):
         if xml is None:
-            with open(fpath, 'r', encoding='utf8') as fp:
+            with open(fpath, "r", encoding="utf8") as fp:
                 xml = fp.read()
         self.init(xml)
         self.adb = adb
-        
-    
+
     def init(self, source, wait_timeout=0.01):
         self.source = source
-        self.settings = {'xpath_debug':False}
+        self.settings = {"xpath_debug": False}
         self.watcher = DummyWatcher()
         self.wait_timeout = wait_timeout
-        
-            
+
     def dump_hierarchy(self):
         return self.source
-    
+
     def __getattr__(self, name):
         pass
-    
+
     def click(self, *a, **k):
         return self.adb.ua2.click(*a, **k)
-    
+
     def move_to(self, *a, **k):
         print(a, k)
 
     def find_xpath_safe(self, x):
-        return find_by_xpath(self, x)    
-    
+        return find_by_xpath(self, x)
+
+
 class SnapShotDevice(DummyDevice):
     def __init__(self, adb):
         self.adb = adb
         self.init(adb.ua2.dump_hierarchy(), 0)
-    
+
     def find_xpath_safe(self, x):
-        return find_by_xpath(self, x)    
+        return find_by_xpath(self, x)
 
     def screenshot(self):
         return self.img
-    
+
     def crop_bounds(self, bounds):
         return bounds_to_rect(bounds).crop_img(pil2cv2(self.img))
-    
+
     def show_bounds(self, bounds):
         show(self.crop_bounds(bounds))
 
+
 class SteadyDevice(DummyDevice):
-    def __init__(self, adb, old_key = None):
+    def __init__(self, adb, old_key=None):
         self.adb = adb
-        self.settings = {'xpath_debug':False}
+        self.settings = {"xpath_debug": False}
         self.watcher = DummyWatcher()
         self.wait_timeout = 0
         self.key = None
         self.refresh()
-    
+
     def get_hash_key(self, xml):
-        t = etree.fromstring(xml.encode('utf8'))
-        e = t.xpath('//node[@class="android.widget.FrameLayout"][@package="com.android.systemui"][@resource-id=""]')
+        t = etree.fromstring(xml.encode("utf8"))
+        e = t.xpath(
+            '//node[@class="android.widget.FrameLayout"][@package="com.android.systemui"][@resource-id=""]'
+        )
         if e:
             e = e[0]
             t.remove(e)
             # xml = t.tostring().decode('utf8')
-            xml = etree.tostring(t, encoding='utf8').decode('utf8')
+            xml = etree.tostring(t, encoding="utf8").decode("utf8")
         key = get_hash(xml)
         return key
-
-
 
     def refresh(self):
         old_key = None
@@ -102,17 +105,17 @@ class SteadyDevice(DummyDevice):
             # key = get_hash(xml_dumped)
             key = self.get_hash_key(xml_dumped)
             if old_key != key and i < max_try - 1:
-                print(f'waiting xml tobe steady:...{i}')
+                print(f"waiting xml tobe steady:...{i}")
                 old_key = key
                 time.sleep(0.1)
             else:
                 self.key = key
                 self.source = xml_dumped
                 break
-    
+
     def find_xpath_safe(self, x):
-        return find_by_xpath(self, x)    
-    
+        return find_by_xpath(self, x)
+
     def find_xpath_all(self, x):
         return self.find_xpath_safe(x).all()
 
@@ -126,32 +129,33 @@ class SteadyDevice(DummyDevice):
 
     def has_xpath(self, x):
         return bool(self.find_xpath_safe(x).all())
-    
 
-    
+
 class TaskSnapShotDevice(SnapShotDevice):
     def __init__(self, adb, task, base_dir):
         adb.switch_app()
         SnapShotDevice.__init__(self, adb)
         self.img = adb.screen_shot()
-        
+
         task.key = task.get_task_key(self)
         task_key_pre = task.pre_same_job_key
         self.task = task
         # print([task.key, task_key_pre])
-        
-        self.changed = task.key is None or task_key_pre is None or task_key_pre != task.key
-        
+
+        self.changed = (
+            task.key is None or task_key_pre is None or task_key_pre != task.key
+        )
+
         if self.changed:
-            task.fpath_screenshot = os.path.join(base_dir , f'{task.id}.png')
+            task.fpath_screenshot = os.path.join(base_dir, f"{task.id}.png")
             self.img = adb.screen_shot()
             cv2.imwrite(task.fpath_screenshot.path, self.img)
-            
-            task.fpath_xml = os.path.join(base_dir , f'{task.id}.xml')
-            with open(task.fpath_xml.path, 'wb') as fp:
-                fp.write(self.source.replace("'\\", '').encode('utf8'))
+
+            task.fpath_xml = os.path.join(base_dir, f"{task.id}.xml")
+            with open(task.fpath_xml.path, "wb") as fp:
+                fp.write(self.source.replace("'\\", "").encode("utf8"))
             task.save()
-    
+
     def perform_operation_of_template(self):
         tpl = self.task.tpl_result
         rect = self.task.rect
@@ -161,51 +165,47 @@ class TaskSnapShotDevice(SnapShotDevice):
         # else:
         #     self.task.status = self.task.EMPTY_RECORD
         # self.task.save()
-    
+
     def CLICK(self, rect):
         self.adb.do_click(rect.center_x, rect.center_y)
-    
+
     def DBCLICK(self, rect):
         self.adb.do_double_click(rect.center_x, rect.center_y)
-        
+
     def LONGCLICK(self, rect):
         self.adb.do_longclick(rect.center_x, rect.center_y, duration=1000)
-        
+
     def CLEAR_PICTURES_WEIXIN_AND_CLICK(self, rect):
-        self.adb.clear_temp_dir(base_dir='/sdcard/Pictures/WeiXin/')
+        self.adb.clear_temp_dir(base_dir="/sdcard/Pictures/WeiXin/")
         self.CLICK(rect)
-    
+
     def NONE(self, rect):
         pass
-    
-    
+
 
 class TaskDumpedDevice(SnapShotDevice):
     def __init__(self, task):
         self.init(task.xml, 0)
         self.img = cv2pil(task.img)
         self.adb = task.job.adb
-        
-    
-    
+
+
 class TaskExecuteDevice(SnapShotDevice):
     def __init__(self, task):
         self.init(task.xml, 0)
         self.mask = task.mask
-        
+
     def match_image(self, tpl):
         l = get_template_points(self.mask, tpl.mask, threshold=0.8)
         if l:
             h, w = tpl.mask.shape[:2]
-            return (l[0][0], l[0][1], l[0][0]+w, l[0][1] + h)
-    
+            return (l[0][0], l[0][1], l[0][0] + w, l[0][1] + h)
+
     def match_xml(self, tpl):
-        xml = tpl.xml.decode('utf8')
+        xml = tpl.xml.decode("utf8")
         e = self.find_xpath_safe(xml).wait()
         return e.bounds
-    
-    
-    
+
     # def __getattr__(self, name):
     #     pass
     #
@@ -216,68 +216,83 @@ class TaskExecuteDevice(SnapShotDevice):
     #         self.step_name = item
     #     return object.__getattribute__(self, item)
 
-    
-    
+
 def find_by_xpath(adb_device, xpath):
-    '''
+    """
     >>> find_by_xpath(DummyDevice('ut/xiaoshipin/page.xml'), '//*[re:match(@text, "开启悬浮窗")][@index="0"]').exists
     True
     >>> find_by_xpath(DummyDevice('ut/xiaoshipin/page.xml'), '//*[re:match(@text, "开启悬浮窗")][@index="0"]').click()
-    '''
-    return XPath(adb_device)(xpath)     
+    """
+    return XPath(adb_device)(xpath)
+
 
 def wait_jianyin_template_search_input_click_ready(adb_device):
-    '''
+    """
     >>> wait_jianyin_template_search_input_click_ready(DummyDevice('ut/xiaoshipin/page.xml'))
     >>> wait_jianyin_template_search_input_click_ready(DummyDevice('ut/jianyin/page_input_click_ready.xml')) is not None
     True
-    '''
-    return find_by_xpath(adb_device, '//com.lynx.component.svg.UISvg/following-sibling::com.bytedance.ies.xelement.input.LynxInputView').wait()
+    """
+    return find_by_xpath(
+        adb_device,
+        "//com.lynx.component.svg.UISvg/following-sibling::com.bytedance.ies.xelement.input.LynxInputView",
+    ).wait()
+
 
 def wait_jianyin_template_search_completed(adb_device):
-    '''
+    """
     >>> wait_jianyin_template_search_completed(DummyDevice('ut/xiaoshipin/page.xml'))
     >>> wait_jianyin_template_search_completed(DummyDevice('ut/jianyin/page_search_finished.xml')) is not None
     True
-    '''
+    """
     return find_by_xpath(adb_device, '//*[@text="筛选"]').wait()
 
+
 def wait_jianyin_search_button_ready(adb_device):
-    '''
+    """
     >>> wait_jianyin_search_button_ready(DummyDevice('ut/xiaoshipin/page.xml'))
     >>> wait_jianyin_search_button_ready(DummyDevice('ut/jianyin/page_input_click_ready.xml'))
     >>> wait_jianyin_search_button_ready(DummyDevice('ut/jianyin/page_search_button_ready.xml')).attrib['bounds']
     '[184,302][821,404]'
-    '''
-    x = find_by_xpath(adb_device, '//com.lynx.tasm.behavior.ui.LynxFlattenUI[@text="搜索"]/preceding-sibling::*')
+    """
+    x = find_by_xpath(
+        adb_device,
+        '//com.lynx.tasm.behavior.ui.LynxFlattenUI[@text="搜索"]/preceding-sibling::*',
+    )
     if x.wait() is not None:
-        return x.all()[-1] 
+        return x.all()[-1]
+
 
 def wait_jianyin_open_template_directly_buttopn(adb_device, timeout=None):
-    '''
+    """
     >>> wait_jianyin_open_template_directly_buttopn(DummyDevice('ut/xiaoshipin/page.xml')) # doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
     NoTemplatePopupException:
     >>> wait_jianyin_open_template_directly_buttopn(DummyDevice('ut/jianyin/page_tpl_direct_open.xml')) is not None
     True
-    '''
-    e = find_by_xpath(adb_device, '//android.widget.Button[@text="打开看看"]').wait(timeout=timeout)
+    """
+    e = find_by_xpath(adb_device, '//android.widget.Button[@text="打开看看"]').wait(
+        timeout=timeout
+    )
     if e is None:
         raise NoTemplatePopupException
     return e
 
+
 def click_if_jianyin_open_template_giveup_history_button_exists(adb_device):
-    '''
+    """
     >>> click_if_jianyin_open_template_giveup_history_button_exists(DummyDevice('ut/xiaoshipin/page.xml'))
     >>> click_if_jianyin_open_template_giveup_history_button_exists(DummyDevice('ut/jianyin/page_giveup.xml'))
     True
-    '''
-    l = find_by_xpath(adb_device, '//*[@text="放弃"][@resource-id="com.lemon.lv:id/tvCancelResume"]').all()
+    """
+    l = find_by_xpath(
+        adb_device, '//*[@text="放弃"][@resource-id="com.lemon.lv:id/tvCancelResume"]'
+    ).all()
     if len(l):
         l[0].click()
         return True
-    
+
 
 if __name__ == "__main__":
     import doctest
+
     print(doctest.testmod(verbose=False, report=False))
--- tool_wx.py
+++ tool_wx.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 2024年2月24日
 
 @author: lenovo
-'''
+"""
 
 import re
 
@@ -12,10 +12,22 @@ from tool_rect import Rect
 from tool_env import bounds_to_rect
 
 
-ptn_session_name = re.compile('\(\d+\)$')
+ptn_session_name = re.compile("\(\d+\)$")
+
+ptn_renamed_session_name = re.compile("_[A-Z]{4}_\(\d+\)$")
+
+
+def is_renamed_session_name(line):
+    """
+    >>> is_renamed_session_name("_SUNY_(5)")
+    True
+    >>> is_renamed_session_name("_1UNY_(5)")
+    False
+    """
+    return bool(ptn_renamed_session_name.match(line.strip()))
 
 def is_session_name(line):
-    '''
+    """
     >>> is_session_name("主流程测试(5)")
     True
     >>> is_session_name("主流程测试(5)1")
@@ -26,11 +38,12 @@ def is_session_name(line):
     False
     >>> is_session_name("11(5)")
     True
-    '''
-    return bool(re.match('.+\(\d+\)$', line.strip()))
+    """
+    return bool(re.match(".+\(\d+\)$", line.strip()))
+
 
 def clean_session_name(line):
-    '''
+    """
     >>> clean_session_name('主流程测试(5)')
     '主流程测试'
     >>> clean_session_name('主流程测试(5)1')
@@ -38,32 +51,33 @@ def clean_session_name(line):
     >>> clean_session_name(None)
     >>> clean_session_name('广州HR同城进阶学习群(56) ')
     '广州HR同城进阶学习群'
-    '''
+    """
     if line:
-        return ptn_session_name.sub('',line.strip())
+        return ptn_session_name.sub("", line.strip())
+
 
 def clean_head_description(line):
-    '''
+    """
     >>> clean_head_description('SunnyAftRain头像')
     'SunnyAftRain'
-    '''
+    """
     return line[:-2]
 
 
 def two_points_bounds_to_rect(line):
-    '''
+    """
     >>> two_points_bounds_to_rect('[201,1569][366,1628]')
     201 366 1569 1628<165, 59>
     >>> two_points_bounds_to_rect(None)
     >>> two_points_bounds_to_rect(numpy.nan)
-    '''
+    """
     if not pandas.isnull(line):
-        left,top,right, bottom = filter(lambda x:x, re.split('[^\d]+', line))
+        left, top, right, bottom = filter(lambda x: x, re.split("[^\d]+", line))
         return Rect(left, right, top, bottom)
-    
+
 
 def is_close_to_top(rect, bounds):
-    '''
+    """
     >>> is_close_to_top(rect_container_chat, "[0,226][1080,308]")
     True
     >>> is_close_to_top(rect_container_chat, "[0,308][1080,745]")
@@ -76,11 +90,12 @@ def is_close_to_top(rect, bounds):
     False
     >>> is_close_to_top(rect_container_chat, "[0,226][1080,557]")
     True
-    '''
+    """
     return abs(rect.top - bounds_to_rect(bounds).top) <= 1
 
+
 def is_close_to_bottom(rect, bounds):
-    '''
+    """
     >>> is_close_to_bottom(rect_container_chat, "[0,226][1080,308]")
     False
     >>> is_close_to_bottom(rect_container_chat, "[0,308][1080,745]")
@@ -93,12 +108,12 @@ def is_close_to_bottom(rect, bounds):
     False
     >>> is_close_to_bottom(rect_container_chat, "[0,226][1080,557]")
     False
-    '''
+    """
     return abs(rect.bottom - bounds_to_rect(bounds).bottom) <= 1
 
 
 def is_not_equal_ignore_null(d1, d2, name):
-    '''
+    """
     >>> is_not_equal_ignore_null(d_up, d_down, 'type')
     False
     >>> is_not_equal_ignore_null(d_up, d_down, 'text')
@@ -115,15 +130,20 @@ def is_not_equal_ignore_null(d1, d2, name):
     False
     >>> is_not_equal_ignore_null({'type':1,}, {'type':-1,}, 'type')
     False
-    '''
-    if pandas.isnull(d1.get(name)) or pandas.isnull(d2.get(name)) or d1.get(name) == d2.get(name):
+    """
+    if (
+        pandas.isnull(d1.get(name))
+        or pandas.isnull(d2.get(name))
+        or d1.get(name) == d2.get(name)
+    ):
         return False
-    if name == 'type' and -1 in (d1.get(name), d2.get(name)):
+    if name == "type" and -1 in (d1.get(name), d2.get(name)):
         return False
-    return True 
+    return True
+
 
 def is_up_down_same(d1, d2):
-    '''
+    """
     >>> is_up_down_same(d_up, d_down)
     True
     >>> is_up_down_same(d_down,d_up)
@@ -132,39 +152,44 @@ def is_up_down_same(d1, d2):
     False
     >>> is_up_down_same(d_up,d_false)
     False
-    '''
-    if is_not_equal_ignore_null(d1, d2, name='type'):
+    """
+    if is_not_equal_ignore_null(d1, d2, name="type"):
         return False
 
-    if is_not_equal_ignore_null(d1, d2, name='text'):
+    if is_not_equal_ignore_null(d1, d2, name="text"):
         return False
 
-    if is_not_equal_ignore_null(d1, d2, name='role'):
+    if is_not_equal_ignore_null(d1, d2, name="role"):
         return False
-    
-    if (d1.get('upside') and d2.get('underside')) or (d2.get('upside') and d1.get('underside')):
-        return True 
-    
-    return False 
+
+    if (d1.get("upside") and d2.get("underside")) or (
+        d2.get("upside") and d1.get("underside")
+    ):
+        return True
+
+    return False
+
 
 def remove_null(d):
-    return {k:v for k, v in d.items() if not pandas.isnull(v)}
-    
+    return {k: v for k, v in d.items() if not pandas.isnull(v)}
+
+
 def merge_up_down(d1, d2):
-    '''
+    """
     >>> merge_up_down(d_up, d_down).get('send_time')
     123
     >>> merge_up_down(d_down, d_up).get('send_time')
     123
-    '''
+    """
     assert is_up_down_same(d1, d2)
     d = remove_null(d1)
     d.update(remove_null(d2))
-    return d    
+    return d
+
 
 def merge_up_down_df(df_up, df_down):
-    l_up = df_up.to_dict('records') 
-    l_down = df_down.to_dict('records')
+    l_up = df_up.to_dict("records")
+    l_down = df_down.to_dict("records")
     d_up = l_up[-1]
     d_down = l_down[0]
     if is_up_down_same(d_up, d_down):
@@ -174,44 +199,50 @@ def merge_up_down_df(df_up, df_down):
         l = l_up + l_down
     return pandas.DataFrame(l)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import doctest
     import numpy
-    rect_container_chat = Rect(0,1080,226,2055)
 
-    d_up = {'type': 1,
-     'big_bounds': '[0,226][1080,308]',
-     'upside': True,
-     'underside': False,
-     'from_icon_des': '钟文栋头像',
-     'from_icon_bounds': '[16,226][156,308]',
-     'role': 1,
-     'text': '第3回测试。',
-     'send_time': pandas.NaT,
-     'bounds': '[156,226][490,292]'}
+    rect_container_chat = Rect(0, 1080, 226, 2055)
 
-    d_down = {'type': 1,
-     'big_bounds': '[0,1975][1080,2055]',
-     'upside': False,
-     'underside': True,
-     'role': 1,
-     'text': '第3回测试。',
-     'bounds': '[156,1991][490,2055]',
-     'from_icon_des': '钟文栋头像',
-     'send_time': 123,
-     'from_icon_bounds': '[16,1975][156,2055]',
-     }
-    
-    d_false = {'type': 3,
-     'big_bounds': '[0,226][1080,557]',
-     'upside': True,
-     'underside': False,
-     'role': 0,
-     'text': 'InfoQ',
-     'bounds': '[167,226][623,541]',
-     'from_icon_des': numpy.nan,
-     'from_icon_bounds': numpy.nan,
-     'send_time': pandas.NaT,
-     }
-    
+    d_up = {
+        "type": 1,
+        "big_bounds": "[0,226][1080,308]",
+        "upside": True,
+        "underside": False,
+        "from_icon_des": "钟文栋头像",
+        "from_icon_bounds": "[16,226][156,308]",
+        "role": 1,
+        "text": "第3回测试。",
+        "send_time": pandas.NaT,
+        "bounds": "[156,226][490,292]",
+    }
+
+    d_down = {
+        "type": 1,
+        "big_bounds": "[0,1975][1080,2055]",
+        "upside": False,
+        "underside": True,
+        "role": 1,
+        "text": "第3回测试。",
+        "bounds": "[156,1991][490,2055]",
+        "from_icon_des": "钟文栋头像",
+        "send_time": 123,
+        "from_icon_bounds": "[16,1975][156,2055]",
+    }
+
+    d_false = {
+        "type": 3,
+        "big_bounds": "[0,226][1080,557]",
+        "upside": True,
+        "underside": False,
+        "role": 0,
+        "text": "InfoQ",
+        "bounds": "[167,226][623,541]",
+        "from_icon_des": numpy.nan,
+        "from_icon_bounds": numpy.nan,
+        "send_time": pandas.NaT,
+    }
+
     print(doctest.testmod(verbose=False, report=False))
