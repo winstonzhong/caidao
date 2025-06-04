55f0fae5fef9a027e6d14c2f20fdf2644ae39018
liqiang <liq10@knownsec.com>
2025-06-04 11:02:05 +0800

    feat:增加prepay函数

--- caidao_tools/mp_weixin/helper_wx.py
+++ caidao_tools/mp_weixin/helper_wx.py
@@ -1,4 +1,5 @@
 import requests
+from wechatpy import WeChatPay
 
 
 def get_login_info(js_code, app_id, app_secret):
@@ -8,3 +9,16 @@ def get_login_info(js_code, app_id, app_secret):
     r = r.json()
     print('r', r)
     return r
+
+
+def wx_prepay(open_id, price, out_trade_no, prepay_info, app_id, api_key, mch_id, notify_url):
+    pay = WeChatPay(appid=app_id, api_key=api_key, mch_id=mch_id)
+    order = pay.order.create(
+        trade_type='JSAPI',
+        user_id=open_id,
+        notify_url=notify_url,
+        total_fee=price,
+        body=prepay_info,
+        out_trade_no=out_trade_no
+    )
+    print(order)
