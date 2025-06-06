f71133285414c281bf47c145454f30de19ecdf32
liqiang <liq10@knownsec.com>
2025-06-06 22:09:37 +0800

    fix: 去除wx_prepay功能

--- caidao_tools/mp_weixin/helper_wx.py
+++ caidao_tools/mp_weixin/helper_wx.py
@@ -1,5 +1,4 @@
 import requests
-from wechatpy import WeChatPay
 
 
 def get_login_info(js_code, app_id, app_secret):
@@ -7,18 +6,7 @@ def get_login_info(js_code, app_id, app_secret):
     params = {'appid': app_id, 'secret': app_secret, 'js_code': js_code, 'grant_type': 'authorization_code'}
     r = requests.get(url=url, params=params)
     r = r.json()
-    print('r', r)
+    # print('r', r)
     return r
 
 
-def wx_prepay(open_id, price, out_trade_no, prepay_info, app_id, api_key, mch_id, notify_url):
-    pay = WeChatPay(appid=app_id, api_key=api_key, mch_id=mch_id)
-    order = pay.order.create(
-        trade_type='JSAPI',
-        user_id=open_id,
-        notify_url=notify_url,
-        total_fee=price,
-        body=prepay_info,
-        out_trade_no=out_trade_no
-    )
-    print(order)
