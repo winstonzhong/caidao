9528578eec815b6879460f6e0527707c294d8ae7
a18190 <a18190@qq.com>
2025-09-04 18:00:55 +0800

    updated

--- tool_dict.py
+++ tool_dict.py
@@ -1,11 +1,11 @@
-'''
+"""
 Created on 2024年7月24日
 
 @author: lenovo
-'''
+"""
 
 class PropDict(dict):
-    '''
+    """
     >>> pd = PropDict()
     >>> pd.aaa = 1
     >>> pd.aaa
@@ -13,16 +13,21 @@ class PropDict(dict):
     >>> pd = PropDict({'a':2,'b':4})
     >>> pd.a
     2
-    '''
+    >>> pd.get = 100
+    >>> pd.get != 100
+    True
+    >>> pd['get'] == 100
+    True
+    >>> pd.bbb
+    """
+
     def __setattr__(self, name, value):
         self[name] = value
-        
-    def __getattr__ (self, name):
+
+    def __getattr__(self, name):
         return self.get(name)
 
-    
-    
-    
 if __name__ == "__main__":
     import doctest
+
     print(doctest.testmod(verbose=False, report=False))
