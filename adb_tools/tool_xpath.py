d5c536d795a71caff8ded64d867dcd673544dca4
a18190 <a18190@qq.com>
2025-12-18 14:31:47 +0800

    updated

--- adb_tools/tool_xpath.py
+++ adb_tools/tool_xpath.py
@@ -1342,12 +1342,16 @@ class 基本任务(抽象持久序列):
         会话名称 = self.device.干净的微信会话名称 if name is None else name
         self.持久对象.数据.setdefault("会话列表", {})[会话名称] = df.to_json()
         self.持久对象.save()
-        
+
     def 清除历史页(self, name=None):
-        self.存储历史页(pandas.DataFrame(columns=["原始时间", "唯一值", "图片key"]), name)
+        self.存储历史页(
+            pandas.DataFrame(columns=["原始时间", "唯一值", "图片key"]), name
+        )
 
     def 合并并存储历史页(self, df, name=None):
         历史页 = self.得到历史页()
+
+        df = tool_wx_df3.截断已存储的历史部分(df, 全局缓存.最后历史时间)
 
         df = tool_wx_df3.合并上下df(历史页, df)
         # 会话名称 = self.device.干净的微信会话名称 if name is None else name
